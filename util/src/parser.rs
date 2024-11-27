1257c3fb51a6912e569aefff91eb1e6edfbb64a9
arthur8L <arthur.lee945@gmail.com>
2024-11-27 10:54:05 -0600

    add simple parser

--- util/src/parser.rs
+++ util/src/parser.rs
@@ -1,7 +1,16 @@
+use std::{fs::File, io::Read};
+
 pub struct AdventParser;
 
 impl AdventParser {
-    pub fn parse(path: std::path::PathBuf) {
-        unimplemented!();
+    pub fn read(path: std::path::PathBuf) -> std::io::Result<String> {
+        let mut file = File::open(path)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        Ok(content)
+    }
+
+    pub fn lines<'a>(src: &'a str) -> Vec<&'a str> {
+        src.lines().collect::<Vec<&'a str>>()
     }
 }
